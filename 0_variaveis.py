cfd11661e75ce28f97ae9e8cd1d8f5f746760eb1
Jian Licio de Oliveira <jian_oliveira12@hotmail.com>
2024-12-27 13:09:31 -0300

    finalização do projeto
    projeto finalizado

---
+++ 0_variaveis.py
@@ -0,0 +1,3 @@
+kernel_de_processamento = "meu_ambiente" #o Kernel que o notebook utilizara - substitua pelo seu aqui
+amostra = 0.9 #será removido x% do df para a mineração de dados inicial 
+#recomendação: usar uma amostra BEM PEQUENA, com o objetivo de averiguar se vai rodar corretamente na sua maquina. depois ir para o valor desejado
